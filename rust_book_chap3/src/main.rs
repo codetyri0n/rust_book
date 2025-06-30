1d144eedda9bf215df6c1f8caa83b033907db652
Sriram <sriramsundararajan83@gmail.com>
2025-06-30 09:17:36 +0530

    C to F temperature conversion

--- rust_book_chap3/src/main.rs
+++ rust_book_chap3/src/main.rs
@@ -1,53 +1,16 @@
 use std::io;
 
 fn main() {
-    let x = 6;
-    let x = x + 1;
-
-    {
-        let x = x * 2;
-        println!("The value of x at the inner scope is : {x}");
-    }
-    println!("The value of x is : {x}");
-
-    let bruh = -34 / 3;
-    println!("Bruhh : {bruh}");
-
-    let tup = (34, true, "scar");
-    let (x, y, z) = tup;
-    println!("{x}, {y}, {z}");
-
-    let arr = [1, 2, 3, 4, 5];
-    println!("Please enter an array index");
-    let mut index = String::new();
+    println!("Enter the temperature in celsius : ");
+    let mut celsius = String::new();
 
     io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
+        .read_line(&mut celsius)
+        .expect("Failed to get the celsius input");
 
-    let index: usize = index.trim().parse().expect("Not a valid index");
+    let number: f32 = celsius.trim().parse().expect("Please enter a valid number");
 
-    let element = arr[index];
+    let result = number * 9.0 / 5.0 + 32.0;
 
-    println!("The number at the specified index is : {element}");
-
-    another_function(5);
-
-    let y = foo();
-    println!("Fooo {y}");
-
-    println!("{}", plus_one(3));
-}
-
-fn another_function(x: i32) {
-    println!("Gimme {x} chocolates.");
-}
-
-fn foo() -> i32 {
-    555
-}
-
-fn plus_one(mut x: i32) -> i32 {
-    x = x + 1;
-    return x;
+    println!("The result in fahrenheit is {result} F");
 }
